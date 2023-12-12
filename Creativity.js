94bf425db7b448dba306e4b6a7f07807e14a356a
Lexton <lextonnorail@gmail.com>
2023-12-13 02:33:02 +0800

    first comit incomplete manga search have bugs

---
+++ Creativity.js
@@ -0,0 +1,48 @@
+var searchbtn = document.getElementById('search_btn');
+var searchInput = document.getElementById('search').value;
+
+searchbtn.addEventListener('click', async (evt) => {
+    var search = document.getElementById('search').value;
+    var results = document.getElementById('manga_result');
+
+    results.innerHTML = "";
+
+    if (search.trim() === "") {
+        return; //Di mo kuhag API rquest ug way input
+    }
+    fetch(`https://api.mangadex.org/manga?title=${search}&includes%5B%5D=cover_art`)
+        .then (response => response.json())
+        .then(data => {
+            var Mangadata = data?.data || []; //Di kuhaon ang undefined nga data
+            Mangadata.forEach(manga => {
+                let mangatitle = manga.attributes.title.en;
+                let mangadesc = manga.attributes.description.en;
+                manga.relationships.forEach((relationship, index) => {  //iterating relationship to find Filename
+                    if (relationship.attributes && relationship.attributes.fileName) { // finding
+                        const fileName = relationship.attributes.fileName;
+                        const mangaid = manga.id;
+                        results.innerHTML += `
+                <div class="manga-container">
+                    <div id="manga-img">
+                        <img src="https://uploads.mangadex.org/covers/${mangaid}/${fileName}"/>
+                    </div>
+                <div id="text-group">
+                    <div id="manga-title">
+                        <h3>${mangatitle}</h3>
+                    </div>
+                    <div id="manga-desc">
+                        <p>${mangadesc}</p>
+                    </div>
+                </div>
+                </div>`;
+
+                    }
+                });
+            });
+
+
+        });
+});
+searchbtn.addEventListener('click', (evt) => {
+    document.getElementById("search").value = "";
+});
