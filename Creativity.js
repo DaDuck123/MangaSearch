300e0ad06011e7d1c782462cd2bdbacd0bb21e84
Lexton <lextonnorail@gmail.com>
2023-12-13 09:27:19 +0800

    added more feature

--- Creativity.js
+++ Creativity.js
@@ -1,5 +1,47 @@
 var searchbtn = document.getElementById('search_btn');
 var searchInput = document.getElementById('search').value;
+var results = document.getElementById('manga_result');
+
+
+fetch(`https://api.mangadex.org/manga?limit=10&includes%5B%5D=cover_art`)
+    .then(response => response.json())
+    .then(data => {
+        var Mangadata = data?.data || []; //Di kuhaon ang undefined nga data
+
+        if (Mangadata.length === 0) {
+            // Display a message when no titles are found
+            results.innerHTML = "<p>No titles found.</p>";
+            return;
+        }
+
+        Mangadata.forEach(manga => {
+            let mangatitle = manga.attributes.title.en;
+            let mangadesc = manga.attributes.description.en;
+            manga.relationships.forEach((relationship, index) => {  //iterating relationship to find Filename
+                if (relationship.attributes && relationship.attributes.fileName) { // finding
+                    const fileName = relationship.attributes.fileName;
+                    const mangaid = manga.id;
+                    results.innerHTML += `
+                <div class="manga-container">
+                    <div id="manga-img">
+                        <img src="https://uploads.mangadex.org/covers/${mangaid}/${fileName}"/>
+                    </div>
+                <div id="text-group">
+                    <div id="manga-title">
+                        <h3>${mangatitle}</h3>
+                    </div>
+                    <div id="manga-desc">
+                        <p>${mangadesc}</p>
+                    </div>
+                </div>
+                </div>`;
+                    console.log(Mangadata)
+                }
+            });
+        });
+
+
+    });
 
 searchbtn.addEventListener('click', async (evt) => {
     var search = document.getElementById('search').value;
@@ -11,7 +53,7 @@ searchbtn.addEventListener('click', async (evt) => {
         return; //Di mo kuhag API rquest ug way input
     }
     fetch(`https://api.mangadex.org/manga?title=${search}&includes%5B%5D=cover_art`)
-        .then (response => response.json())
+        .then(response => response.json())
         .then(data => {
             var Mangadata = data?.data || []; //Di kuhaon ang undefined nga data
 
@@ -20,7 +62,7 @@ searchbtn.addEventListener('click', async (evt) => {
                 results.innerHTML = "<p>No titles found.</p>";
                 return;
             }
-    
+
             Mangadata.forEach(manga => {
                 let mangatitle = manga.attributes.title.en;
                 let mangadesc = manga.attributes.description.en;
